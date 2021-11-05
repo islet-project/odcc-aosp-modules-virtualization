Droid-VM/Virtualization

authfs: support remote output directory
This change contains 3 major groups:
 - authfs/{aidl, fd_server}: new AIDL API and the service implementation
 - authfs/src: implement FUSE APIs for creating directory and file, by
               interact with the new service API as a client
 - authfs/tests, tests/: test coverage

A few notable changes that might help reviewing:

 - Now that both AuthFs and FdService struct is no longer immutable (in
   order to allow writable directory), their BTreeMap are now guarded by
   Arc<Mutex<_>>.

   * AuthFs::insert_new_inode and FdService::insert_new_fd are designed
     specifically to allow querying then mutating the map, which isn't
     trivial.

 - File and directory modes from the user program / VFS are currently
   ignored (just not to grow the change size).

 - Some shuffling of test paths to make it easy to clean up in tearDown.

Bug: 203251769
Test: AuthFsHostTest
Change-Id: I50f3f1ba8a3ebd969cf0f25a8feab2ec8cb1a2dc

authfs/aidl/com/android/virt/fs/IVirtFdService.aidl
@@ -53,5 +53,22 @@ interface IVirtFdService {
     /** Resizes the file backed by the given file ID to the new size. */
     void resize(int id, long size);
 
+    /** Returns the file size. */
     long getFileSize(int id);
+
+    /**
+     * Create a file given the directory ID.
+     *
+     * @param basename The file name to create. Should not contain directory separator.
+     * @return file ID that represent the new created file.
+     */
+    int createFileInDirectory(int id, String basename);
+
+    /**
+     * Create a directory inside the given directory ID.
+     *
+     * @param basename The directory name to create. Should not contain directory separator.
+     * @return file ID that represent the new created directory.
+     */
+    int mkdir(int id, String basename);
 }

islet-project/odcc-aosp-modules-virtualization

authfs/aidl/com/android/virt/fs/IVirtFdService.aidl
@@ -53,5 +53,22 @@ interface IVirtFdService {
     /** Resizes the file backed by the given file ID to the new size. */
     void resize(int id, long size);
 
+    /** Returns the file size. */
     long getFileSize(int id);
+
+    /**
+     * Create a file given the directory ID.
+     *
+     * @param basename The file name to create. Must not contain directory separator.
+     * @return file ID that represents the new created file.
+     */
+    int createFileInDirectory(int id, String basename);
+
+    /**
+     * Create a directory inside the given directory ID.
+     *
+     * @param basename The directory name to create. Must not contain directory separator.
+     * @return file ID that represents the new created directory.
+     */
+    int createDirectoryInDirectory(int id, String basename);
 }